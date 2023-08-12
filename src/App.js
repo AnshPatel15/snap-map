a0ba0a42c8e2ffd0ea76b9dbfb1c675b79177e5a
AnshPatel15 <anjalasnsh@gmail.com>
2023-08-12 18:49:13 +0530

    mapbox error fixed

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import BoxCreate from "./components/BoxCreate";
 function App() {
   return (
     <div className=" text-center bg-gray-700 min-h-screen">
-      {" "}
       <span className=" text-white text-3xl">Snap-Map Project</span>
       <div className="flex flex-col gap-3 justify-between p-5">
-        <div className=" h-64 sm:h-96 md:h-128 lg:h-160 xl:h-[90vh] w-full xl:w-[90vw]  xl:ml-[67px] self-center border-4 rounded-md border-blue-800">
+        <div className="h-64 sm:h-96 md:h-128 lg:h-160 xl:h-[90vh] w-full xl:w-[90vw]  xl:ml-[67px] self-center border-4 rounded-md border-blue-800">
           <MapBox />
         </div>
         <div className="hover:border-2 hover:border-blue-800 self-center">
--- src/components/MapBox.jsx
+++ src/components/MapBox.jsx
@@ -3,9 +3,6 @@ import Map, { GeolocateControl, Marker, NavigationControl } from "react-map-gl";
 import { useState } from "react";
 
 const MapBox = () => {
-  // get screenshot of map here
-
-  // default viewport settings
   const [viewPort, setViewPort] = useState({
     width: "100 %",
     height: "100%",
@@ -14,13 +11,11 @@ const MapBox = () => {
     zoom: 6,
   });
 
-  //  default marker location
   const [markerLocation, setMarkerLocation] = useState({
     latitude: 28.6448,
     longitude: 77.216,
   });
 
-  // onClick for geolocate
   const handleGeolocate = (position) => {
     setMarkerLocation({
       latitude: position.coords.latitude,
@@ -29,30 +24,26 @@ const MapBox = () => {
   };
 
   return (
-    <div className=" relative h-full w-full">
-      <div className=" h-full w-full">
-        <Map
-          onMove={(evt) => setViewPort(evt.viewState)}
-          {...viewPort}
-          mapboxAccessToken={process.env.REACT_APP_MAP}
-          mapStyle="mapbox://styles/ansh225/clkqvfw9600o201pbh3l0ht8q"
-        >
-          <Marker
-            latitude={markerLocation.latitude}
-            longitude={markerLocation.longitude}
-          />
+    <Map
+      onMove={(evt) => setViewPort(evt.viewState)}
+      {...viewPort}
+      mapboxAccessToken={process.env.REACT_APP_MAP}
+      mapStyle="mapbox://styles/ansh225/clkqvfw9600o201pbh3l0ht8q"
+    >
+      <Marker
+        latitude={markerLocation.latitude}
+        longitude={markerLocation.longitude}
+      />
 
-          <NavigationControl position="bottom-right" />
-          <GeolocateControl
-            position="top-left"
-            positionOptions={{ enableHighAccuracy: true }}
-            trackUserLocation={true}
-            showUserHeading={true}
-            onGeolocate={handleGeolocate}
-          />
-        </Map>
-      </div>
-    </div>
+      <NavigationControl position="bottom-right" />
+      <GeolocateControl
+        position="top-left"
+        positionOptions={{ enableHighAccuracy: true }}
+        trackUserLocation={true}
+        showUserHeading={true}
+        onGeolocate={handleGeolocate}
+      />
+    </Map>
   );
 };
 
